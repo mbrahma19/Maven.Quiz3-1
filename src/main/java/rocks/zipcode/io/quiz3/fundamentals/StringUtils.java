53f6a9803ff5d3b521a34de7d085ce9649998ace
Marshilla Brahma <marshillabrahma@zipcoders-MacBook-Pro-10.local>
2019-03-25 11:16:57 -0400

    Reverting back code for updated tests

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/StringUtils.java
@@ -32,6 +32,6 @@ public class StringUtils {
     }
 
     public static Integer getNumberOfSubStrings(String input){
-        return (input.length()*(input.length()+1))/2;
+        return getAllSubStrings(input).length;
     }
 }
